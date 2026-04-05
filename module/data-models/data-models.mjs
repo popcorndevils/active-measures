215ff35e32a995efc48d46295fef70e5c375a6ad
Michael Coates <popcorndevils@gmail.com>
2026-04-05 11:25:19 -0400

    - update git ignore to exclude binaries from packs - adding paths to system definition - removing legacy commented out code from datamodels to be added at appropriate times.

--- module/data-models/data-models.mjs
+++ module/data-models/data-models.mjs
@@ -1,44 +1,8 @@
 // const { HTMLField, NumberField, SchemaField, StringField } = foundry.data.fields;
 
 /* -------------------------------------------- */
-/*  Actor Models                                */
+/*  Actors                                      */
 /* -------------------------------------------- */
 
 export {default as ActorNpc} from "./actors/ActorNpc.mjs";
 export {default as ActorPlayer} from "./actors/ActorPlayer.mjs";
-
-/* -------------------------------------------- */
-/*  Item Models                                 */
-/* -------------------------------------------- */
-
-// class ItemDataModel extends foundry.abstract.TypeDataModel {
-//   static defineSchema() {
-//     return {
-//       rarity: new StringField({
-//         required: true,
-//         blank: false,
-//         options: ["common", "uncommon", "rare", "legendary"],
-//         initial: "common"
-//       }),
-//       price: new NumberField({ required: true, integer: true, min: 0, initial: 20 })
-//     };
-//   }
-// }
-
-// export class WeaponDataModel extends ItemDataModel {
-//   static defineSchema() {
-//     return {
-//       ...super.defineSchema(),
-//       damage: new NumberField({ required: true, integer: true, positive: true, initial: 5 })
-//     };
-//   }
-// }
-
-// export class SpellDataModel extends ItemDataModel {
-//   static defineSchema() {
-//     return {
-//       ...super.defineSchema(),
-//       cost: new NumberField({ required: true, integer: true, positive: true, initial: 2 })
-//     };
-//   }
-// }
